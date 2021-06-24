f728775e7f8a428bb7312a0e5854c9585e075b62
Ankit_ Anmol <>
2021-06-24 10:11:43 +0000

    POST works

--- Keep.py
+++ Keep.py
@@ -1,9 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from threading import Thread
 app = Flask('app')
 
-@app.route("/delete")
+@app.route("/delete",methods = ["POST","GET"])
 def delete():
+  if request.method == "POST":
+    link = request.form['LinkID']
+    print(link)
+  elif "LinkID" in request.args:
+    print(request.args['LinkID'])
   return render_template("Delete.html")
 
 
